2efe3a6f98235253e9ccf25fe684fb4dee3c23d6
kawseribn <kawser.ibn.93@gmail.com>
2021-09-02 19:40:42 +0600

    adding analyze.py

--- generate.py
+++ generate.py
@@ -1,12 +1,25 @@
 import pyrosim.pyrosim as pyrosim
 
 
-pyrosim.Start_SDF("boxes.sdf")
-#pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[x,y,z])
-for i in range(6):
-    for j in range(6):
-        x,y,z = 1,1,1
-        for k in range(10):
-            pyrosim.Send_Cube(name="Box", pos=[i,j,0.5+k] , size=[x,y,z])
-            x,y,z = 0.9*x,0.9*y,0.9*z
-pyrosim.End()
+def Create_World():
+    x,y,z = 1,1,1
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[2,2,0.5] , size=[x,y,z])
+    pyrosim.End()
+
+def Create_Robot():
+    x,y,z = 1,1,1
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5] , size=[x,y,z])
+    pyrosim.Send_Joint( name = "Torso_Leg1" , parent= "Torso" , child = "BackLeg" , 
+        type = "revolute", position = "1 0.0 1")
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5] , size=[x,y,z])
+    pyrosim.Send_Joint( name = "Torso_Leg2" , parent= "Torso" , child = "FrontLeg" , 
+        type = "revolute", position = "2 0.0 1")
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5] , size=[x,y,z])
+    
+    pyrosim.End()
+
+
+Create_World()
+Create_Robot()
--- simulate.py
+++ simulate.py
@@ -1,15 +1,31 @@
 import pybullet as p
 import time
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
+import numpy as np
 
 physicsClient = p.connect(p.GUI)
 
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("boxes.sdf")
+planeId = p.loadURDF("body.urdf")
 
-for i in range(1000):
+p.loadSDF("world.sdf")
+
+pyrosim.Prepare_To_Simulate("body.urdf")
+backLegSensorValues = np.zeros(100)
+frontLegSensorValues = np.zeros(100)
+
+#print(backLegSensorValues)
+#exit()
+for i in range(100):
     p.stepSimulation()
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     time.sleep(1/60)
 p.disconnect()
+print(backLegSensorValues)
+print(frontLegSensorValues)
+np.save('data/backLegSensorValues.npy',backLegSensorValues)
+np.save('data/frontLegSensorValues.npy',frontLegSensorValues)
