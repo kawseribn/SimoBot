994de6926e62ce502e601fdbc37354ccc5c7048d
kawseribn <kawser.ibn.93@gmail.com>
2021-09-02 14:55:44 +0600

    adding many links

--- generate.py
+++ generate.py
@@ -1,5 +1,12 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+
+pyrosim.Start_SDF("boxes.sdf")
+#pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[x,y,z])
+for i in range(6):
+    for j in range(6):
+        x,y,z = 1,1,1
+        for k in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[i,j,0.5+k] , size=[x,y,z])
+            x,y,z = 0.9*x,0.9*y,0.9*z
 pyrosim.End()
