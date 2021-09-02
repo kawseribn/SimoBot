3e63d10ad636ee12c116e2fff9cb6aa82884c684
kawseribn <kawser.ibn.93@gmail.com>
2021-09-02 18:22:44 +0600

    adding two joints to the torso

--- simulate.py
+++ simulate.py
@@ -7,7 +7,9 @@ physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("boxes.sdf")
+planeId = p.loadURDF("body.urdf")
+
+p.loadSDF("world.sdf")
 
 for i in range(1000):
     p.stepSimulation()
