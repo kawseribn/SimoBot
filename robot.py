f883df363d1c25c86c7b48a779e896eaa7c6e734
kawseribn <kawser.ibn.93@gmail.com>
2021-09-04 18:31:53 +0600

    adding neurons

--- robot.py
+++ robot.py
@@ -1,5 +1,9 @@
 import pyrosim.pyrosim as pyrosim
 import pybullet as p
+
+from pyrosim.neuralNetwork import NEURAL_NETWORK
+
+
 from sensor import SENSOR
 from motor import MOTOR
 import constants as c
@@ -9,6 +13,7 @@ class ROBOT:
         self.motors = {}
         self.robot = p.loadURDF("body.urdf")
         pyrosim.Prepare_To_Simulate("body.urdf")
+        self.nn = NEURAL_NETWORK("brain.nndf")
         self.Prepare_To_Sense()
         self.Prepare_To_Act()
 
@@ -27,12 +32,26 @@ class ROBOT:
     def Prepare_To_Act(self):
         for jointName in pyrosim.jointNamesToIndices:
             self.motors[jointName] = MOTOR(jointName)
-            if int(jointName[-1])%2==0:
-                print(self.motors[jointName].frequency)
+            #if int(jointName[-1])%2==0:
+            if 'BackLeg' in jointName:
+                #print(self.motors[jointName].frequency)
                 self.motors[jointName].frequency = c.LegFrequency/2
-                print(self.motors[jointName].frequency)
+                #print(self.motors[jointName].frequency)
             #print(jointName)
     def ACT(self,t):
-        for i in self.motors.keys():
+        for neuronName in self.nn.Get_Neuron_Names():
+            if self.nn.Is_Motor_Neuron(neuronName):
+                desiredAngle = self.nn.Get_Value_Of(neuronName)
+
+                jointName = self.nn.Get_Joint_Name(neuronName)
+                self.motors[jointName].Set_Value(desiredAngle,self.robot)
+                print(neuronName,desiredAngle)
+                
+        '''for i in self.motors.keys():
             self.motors[i].Set_Value(t,self.robot)
-            #print(self.motors[i])
+            #print(self.motors[i])'''
+
+    def Think(self):
+        self.nn.Update()
+        self.nn.Print()
+        
