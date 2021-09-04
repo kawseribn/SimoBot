5ddbe973cac9b42341fbf5df5cc1cd922fcbf6fa
kawseribn <kawser.ibn.93@gmail.com>
2021-09-04 20:45:36 +0600

    Adding synapses

--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -69,10 +69,19 @@ class NEURON:
     def Update_Sensor_Neuron(self):
         self.Set_Value(pyrosim.Get_Touch_Sensor_Value_For_Link(self.Get_Link_Name()))
         
-    def Update_Hidden_Or_Motor_Neuron(self):
+    def Update_Hidden_Or_Motor_Neuron(self, neurons, synapses):
         #self.Set_Value(math.pi/4.0)
         self.Set_Value(0.0)
-
+        print(self.Get_Value())
+        for key in synapses.keys():
+            if key[1] == self.Get_Name():
+                self.Allow_Presynaptic_Neuron_To_Influence_Me(synapses[key].Get_Weight(),neurons[key[0]].Get_Value())
+        self.Threshold()
+        '''print(self.Get_Value())
+        exit()'''
+    def Allow_Presynaptic_Neuron_To_Influence_Me(self, synapsesWeight, presynapticsValue):
+        
+        self.Add_To_Value(synapsesWeight*presynapticsValue)
 # -------------------------- Private methods -------------------------
 
     def Determine_Name(self,line):
